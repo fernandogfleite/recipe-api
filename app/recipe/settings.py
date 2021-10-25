b5d82944a2637bb47279ac0ff8aa0a581f215f60
Fernando Leite <fernando.leite@wlcsolucoes.com.br>
2021-10-25 17:01:18 -0300

    Adição do app user no projeto

--- app/recipe/settings.py
+++ app/recipe/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'recipe.apps.core.apps.CoreConfig',
+    'recipe.apps.user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
